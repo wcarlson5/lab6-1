13b724f9bc4dcb7a6ee102442133e7b0cf7033dd
Walker Carlson <wcarlson@ucsd.edu>
2019-02-14 10:13:35 -0800

    lab done

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -37,7 +37,7 @@ function callback(result){
     '</small></p><p><small>' + result['summary'] +
     '</small></p></a>';
 	console.log(projectHTML);
-	$(".project #"+result.id).html("projectHTML");
+	$(".project #project"+result.id).html("projectHTML");
 
 
 
