c09fba1b575c18830ccbbc10c69a1e6961c82966
Walker Carlson <wcarlson@ucsd.edu>
2019-02-14 09:54:25 -0800

    lab done

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -25,7 +25,7 @@ function addProjectDetails(e) {
 	var projectID = $(this).closest('.project').attr('id');
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
-	$("a").get("/project/"+idNumber,callback);
+	$.get("/project/"+idNumber,callback);
 	console.log("User clicked on project " + idNumber);
 }
 
