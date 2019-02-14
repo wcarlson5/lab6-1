675e0614685c6832e59b7cfb5a7e0e2a26fc948b
Walker Carlson <wcarlson@ucsd.edu>
2019-02-13 19:13:09 -0800

    pri

--- public/js/facebook.js
+++ public/js/facebook.js
@@ -17,3 +17,12 @@ function statusChangeCallback(response) {
          FB.api('/me?fields=name,first_name,picture.width(480)', changeUser);
   }
 }
+
+//Add this callback at bottom of facebook.js and add the required functionality in it 
+function changeUser(response) {
+  //Add code to change name and image
+  $(.facebookLogin).hide(); 
+  $(#name).text(response.name); 
+  $(#photo).attr("src",response.picture.data.url);
+
+}
